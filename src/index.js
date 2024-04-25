0c335201c5c5828d1119d2e50d1531ffc739d26b
SaeedAbi <abdary.saeed@gmail.com>
2024-04-25 16:12:55 +0330

    🎉 starting new App

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import reactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+const root = reactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+// react.render(<App />);
